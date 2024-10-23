bad859d29c9a7f192816f4fe201f2af869b51e6a
DubaKanu <j.kanu@alustudent.com>
2024-10-23 14:33:42 +0200

    first ten subreddit

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,41 +1,28 @@
 #!/usr/bin/python3
 """Queries the Reddit API and prints top ten hot posts."""
-import json
 import requests
-import sys
 
 
 def top_ten(subreddit):
     """Print titles of the first 10 hot posts for a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0.1.0 (by /u/your_username)"
     }
     params = {
         "limit": 10
     }
-
     try:
-        response = requests.get(
-            url,
-            headers=headers,
-            params=params,
-            allow_redirects=False
-        )
-
-        if response.status_code != 200:
+        r = requests.get(url, headers=headers,
+                        params=params, allow_redirects=False)
+        if r.status_code != 200:
             print(None)
-            return
-
-        data = response.json().get("data", {})
-        children = data.get("children", [])
-
-        if not children:
-            print(None)
-            return
-
-        for child in children:
-            print(child.get("data", {}).get("title", ""))
-
+        else:
+            data = r.json().get("data", {})
+            children = data.get("children", [])
+            if not children:
+                print(None)
+            for post in children:
+                print(post.get("data", {}).get("title", ""))
     except Exception:
         print(None)
