30888d0518ae55016da7fb0cba888c65f55c6666
DubaKanu <j.kanu@alustudent.com>
2024-10-24 18:03:20 +0200

    number of subscriber

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -16,5 +16,5 @@ def number_of_subscribers(subreddit):
     if response.status_code == 200:
         data = response.json()["data"]
         return data["subscribers"]
-    else:
+    else:     
         return 0
