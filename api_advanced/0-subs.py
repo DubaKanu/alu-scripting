c7f1aabf84a1556cdb04d7d53cb370f6a7a9d331
DubaKanu <j.kanu@alustudent.com>
2024-10-24 17:52:17 +0200

    number of subscribers

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python3
-"""This module contains functions to interact with the Reddit API."""
+"""
+function that queries the 'Reddit API' and returns the number of subscribers
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Query the Reddit API and return the number of subscribers for a given subreddit."""
-    # Reddit API URL
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    """
+    number of subscribers
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "MyBot/1.0"}  # To avoid errors
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    # Custom headers to avoid Too Many Requests error
-    headers = {
-        'User-Agent': 'linux:0.1.0 (by /u/your_username)'
-    }
-
-    try:
-        # Make GET request to Reddit API
-        response = requests.get(url, headers=headers, allow_redirects=False)
-
-        # Check if request was successful
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-
-    except Exception:
+    if response.status_code == 200:
+        data = response.json()["data"]
+        return data["subscribers"]
+    else:
         return 0
